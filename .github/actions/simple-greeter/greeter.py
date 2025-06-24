1c2344b9598bab975750807bbdb05a1c9ca69ec5
vinodhdevaarul <vinodhdevaarul@gmail.com>
2025-06-24 10:34:58 -0500

    push

--- .github/actions/simple-greeter/greeter.py
+++ .github/actions/simple-greeter/greeter.py
@@ -1,10 +1,8 @@
 import sys
 
 def greet(name):
-    if not name:
-        return "Hello, World!"
     return f"Hello, {name}!"
 
 if __name__ == "__main__":
-    name = sys.argv[1] if len(sys.argv) > 1 else "No-one"
+    name = sys.argv[1]
     print(greet(name))
